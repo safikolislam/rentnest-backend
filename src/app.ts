ff6a3a0a920d40919844df369fe9ee0de0b1aa29
Safikol Islam <safikolislam72@gmail.com>
2026-08-28 21:24:42 +0600

    setup prisma and express server

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express,{Application, Request, Response} from "express"
+
+
+const app:Application = express();
+app.get("/",(req:Request,res:Response)=>{
+res.send("rentnest server is running")
+})
+export default app;
