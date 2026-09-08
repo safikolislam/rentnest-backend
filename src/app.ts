7766ecea908a37901e64708103cbe344dc76f778
Safikol Islam <safikolislam72@gmail.com>
2026-09-08 19:57:12 +0600

    test: verify rental request module endpoints working correctly

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser"
 import { categoryRoute } from "./modules/category/category.route";
 import { propertyRoute } from "./modules/property/property.route";
 import { landlordPropertyRoute } from "./modules/property/landlordProperty.route";
+import { rentalRequestRoutes } from "./modules/rentalRequest/rentalRequest.route";
 
 const app: Application = express();
 app.use(express.json());
@@ -22,4 +23,6 @@ app.use("/api/properties",propertyRoute);
 
 app.use("/api/landlord/properties",landlordPropertyRoute)
 
+
+app.use("/api",rentalRequestRoutes)
 export default app;
